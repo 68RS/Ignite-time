150584746865e86c0b9b1dc069dcef92d8b83e65
Robson Santos <rboxe68@gmail.com>
2023-11-10 20:09:29 -0300

    Atualização

--- Projeto-02/src/pages/Home/index.tsx
+++ Projeto-02/src/pages/Home/index.tsx
@@ -2,6 +2,8 @@ import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
+import { useState } from 'react'
+
 import {
   CountdownContainer,
   FormContainer,
@@ -14,27 +16,66 @@ import {
 
 // controlled / uncontrolled
 
+//           Exemplo
 //  function register (name: string) {
 //  return {
 //     onChenge: () => void,
 //     onBlur: () => void,
 //     onFocus: () => void,
+//     ...,
 //  }
 //  }
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
-  MinutesAmount: zod.number().min(5).max(60),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
 })
 
+interface NewCycleFormData {
+  task: ''
+  minutesAmount: number
+}
+
+// type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+interface Cycle {
+  id: string
+  task: string
+  mintesAmount: number
+}
+
 export const Home = () => {
-  const { register, handleSubmit, watch } = useForm({
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
 
-  const handleCreateNewCycle = (data: any) => {
-    console.log(data)
+  const handleCreateNewCycle = (data: NewCycleFormData) => {
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      mintesAmount: data.minutesAmount,
+    }
+
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+
+    reset()
   }
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+  console.log(activeCycle)
 
   const task = watch('task')
   const isSubmitDesabled = !task
@@ -66,7 +107,7 @@ export const Home = () => {
             step={5}
             min={5}
             max={60}
-            {...register('minutesAmont', { valueAsNumber: true })}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos</span>
